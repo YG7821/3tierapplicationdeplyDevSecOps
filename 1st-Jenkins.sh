be7f29525276007dc117c59f64876dbd865e8a1e
YG7821 <yogyata_yogita@yahoo.co.in>
2026-09-07 11:52:14 +0530

    Enhance Jenkins installation script with JRE/JDK steps
    Added installation steps for JRE and JDK, updated Jenkins keyring and source list.

--- 1st-Jenkins.sh
+++ 1st-Jenkins.sh
@@ -20,3 +20,31 @@ sudo apt-get update -y
 
 # Install Jenkins
 sudo apt-get install jenkins -y
+
+-----------------------------------------------------------------------
+# https://www.jenkins.io/doc/book/installing/linux/
+
+
+sudo apt update
+FOR JRE                    sudo apt install fontconfig openjdk-21-jre -y
+FOR JDK                    sudo apt install openjdk-21-jdk
+
+java -version
+
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+
+sudo apt install jenkins -y
+
+jenkins --version
+
+systemctl status jenkins
+systemctl stop jenkins
+systemctl start jenkins
